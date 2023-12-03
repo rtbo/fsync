f141000d5c67769582909286ebd112821efebd67
Rémi Thebault <remi.thebault@gmail.com>
2023-12-03 23:11:59 +0100

    change test folder

--- fsyncd/src/main.rs
+++ fsyncd/src/main.rs
@@ -6,7 +6,7 @@ use fsync::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let st = Arc::new(fs::Storage::new("/home/remi/dev"));
+    let st = Arc::new(fs::Storage::new("/home/remi/Documents"));
     let cache = Cache::new();
     cache.populate(st).await?;
     cache.print_tree();
