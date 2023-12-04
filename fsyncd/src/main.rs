b2800b7f8cdc9eb7e48281c066f9b07ce3131cb3
Rémi Thebault <remi.thebault@gmail.com>
2023-12-04 07:29:39 +0100

    add config

--- fsyncd/src/main.rs
+++ fsyncd/src/main.rs
@@ -1,4 +1,5 @@
 mod cache;
+mod config;
 
 use std::sync::Arc;
 
@@ -6,9 +7,12 @@ use fsync::fs;
 use fsync::Result;
 
 use crate::cache::Cache;
+use crate::config::Config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let config = Config::new();
+
     let st = Arc::new(fs::Storage::new("/home/remi/Documents"));
     let cache = Cache::new();
     cache.populate(st).await?;
