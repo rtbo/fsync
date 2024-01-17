0a64000c052a3f4c0303cf7a0edc5bd39bb90d47
Remi THEBAULT <remi.thebault@gmail.com>
2024-01-17 01:38:43 +0100

    PersistCache default impl

--- fsyncd/src/lib/lib.rs
+++ fsyncd/src/lib/lib.rs
@@ -40,7 +40,9 @@ pub mod uri {
 }
 
 pub trait PersistCache {
-    fn persist_cache(&self) -> impl Future<Output = anyhow::Result<()>> + Send;
+    fn persist_cache(&self) -> impl Future<Output = anyhow::Result<()>> + Send {
+        future::ready(Ok(()))
+    }
 }
 
 pub trait ShutdownObj: Send + Sync + 'static {
